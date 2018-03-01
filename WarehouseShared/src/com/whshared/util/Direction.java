3b24f9cfc2bb21810c535e53b2ba359111b6bf52
extremespancake <extremespancake@googlemail.com>
2018-03-01 22:44:55 +0000

    Created direction class with static final fields

---
+++ WarehouseShared/src/com/whshared/util/Direction.java
@@ -0,0 +1,10 @@
+package com.whshared.util;
+
+public final class Direction {
+	
+	public static final byte FORWARD = 0;
+	public static final byte LEFT = 1;
+	public static final byte RIGHT = 2;
+	public static final byte BACKWARDS = 3;
+	
+}
